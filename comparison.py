a41d5433e9ec8d23e9122cbbe57459e0120071b2
Ismail Sunni <imajimatika@gmail.com>
2019-07-04 17:57:16 +0200

    Add creating bar chart with stds from data.

--- comparison.py
+++ comparison.py
@@ -43,9 +43,30 @@ data = [
     ]
 ]
 
-means = [np.mean(x[1]) for x in data]
-stds = [np.std(x[1]) for x in data]
-labels = [x[0] for x in data]
+def generate_means_stds_labels(data):
+    """
+    Generate dictionary of means, stds, and labels from data.
+    Data is a list with this format:
+    data = [
+        [
+            'label',
+            []  # np.array
+        ],
+        [
+            'label',
+            []  # np.array
+        ],
+    ]
+    """
+    means = [np.mean(x[1]) for x in data]
+    stds = [np.std(x[1]) for x in data]
+    labels = [x[0] for x in data]
+
+    return {
+        'means': means,
+        'stds': stds,
+        'labels': labels
+    }
 
 def bar_chart_with_std(means, stds, labels, title, y_label, file_name=''):
     """
@@ -75,4 +96,12 @@ def bar_chart_with_std(means, stds, labels, title, y_label, file_name=''):
         plt.savefig(file_name)
     plt.show()
 
-bar_chart_with_std(means, stds, labels, title='Title', y_label='Y Label', file_name='')
+def bar_chart_from_data(data, title, y_label, file_name=''):
+    """
+    Create bar chart from a data. See generate_means_stds_labels for the format of the data.
+    """
+    summary = generate_means_stds_labels(data)
+    bar_chart_with_std(summary['means'], summary['stds'], summary['labels'], title, y_label, file_name)
+
+if __name__ == "__main__":
+    bar_chart_from_data(data, title='Title', y_label='Y label')
